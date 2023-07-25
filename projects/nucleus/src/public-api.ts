983d2738b07f3602a3fbfc7acdb2af6318e62f01
Giovanni Roos <giovanniroos@gmail.com>
2023-07-25 17:17:19 +0200

    Comments on public api

--- projects/nucleus/src/public-api.ts
+++ projects/nucleus/src/public-api.ts
@@ -1,11 +1,11 @@
 /*
  * Public API Surface of nucleus
  */
-
+//lib root
 export * from './lib/nucleus.service';
 export * from './lib/nucleus.component';
 export * from './lib/nucleus.module';
-
+//task module
 export * from './lib/tasks/tasks.module';
 export * from './lib/tasks/components/task/task.component';
 export * from './lib/tasks/services/task-store.service';
